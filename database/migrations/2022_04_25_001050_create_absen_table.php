b170ef9330c689c1ae8ca6c671de14673318d535
tataadn <adine.pramesti@gmail.com>
2022-04-25 08:39:54 +0700

    fixing databases

--- database/migrations/2022_04_25_001050_create_absen_table.php
+++ database/migrations/2022_04_25_001050_create_absen_table.php
@@ -16,8 +16,8 @@ return new class extends Migration
         Schema::create('absen', function (Blueprint $table) {
             $table->increments('id_absen');
             $table->date('tanggal');
-            $table->string('id_kelas',10);
-            $table->foreign('id_kelas')->references('id_kelas')->on('kelas');
+            $table->string('id_jadwal',10);
+            $table->foreign('id_jadwal')->references('id_jadwal')->on('jadwal');
             $table->integer('nis');
             $table->foreign('nis')->references('nis')->on('siswa');
             $table->string('lokasi',255);
