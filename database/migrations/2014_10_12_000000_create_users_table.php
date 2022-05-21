85f93bbc4c6062a9cee3e7750fd6f6c088038580
nuranoer <nuranoer7@gmail.com>
2022-05-21 20:28:35 +0700

    fixing error

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -19,6 +19,8 @@ return new class extends Migration
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->tinyInteger('role')->default(0);
+            /* Users: 0=>Siswa, 1=>Guru */
             $table->rememberToken();
             $table->timestamps();
         });
