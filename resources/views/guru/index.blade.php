b6e6a48fabb2bbcf6bb36a8cd8bb3b0c33be1b00
Rexana Widyadhana Agusta <agustaxw@gmail.com>
2022-06-05 21:59:27 +0700

    edit index

--- resources/views/guru/index.blade.php
+++ resources/views/guru/index.blade.php
@@ -32,15 +32,8 @@
                             <div class="col">
                                 <br>
                                 <h5 style="line-height: 1cm">
-                                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime mollitia,
-                                    molestiae quas vel sint commodi repudiandae consequuntur voluptatum laborum
-                                    numquam blanditiis harum quisquam eius sed odit fugiat iusto fuga praesentium
-                                    optio, eaque rerum! Provident similique accusantium nemo autem. Veritatis
-                                    obcaecati tenetur iure eius earum ut molestias architecto voluptate aliquam
-                                    nihil, eveniet aliquid culpa officia aut! Impedit sit sunt quaerat, odit,
-                                    tenetur error, harum nesciunt ipsum debitis quas aliquid. Reprehenderit,
-                                    quia. Quo neque error repudiandae fuga? Ipsa laudantium molestias eos 
-                                    sapiente officiis modi at sunt excepturi
+                                    TATA CANTIK INSTRINYA TAEHYUNG
+                                    ANAKNYA PAK ADNAN
                                 </h5>
                             </div>
                         </div>
